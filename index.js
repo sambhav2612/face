59ff159caead231de1db3625d2e50cf57b577c47
Sambhav Jain <sambhavjain2612@gmail.com>
2018-06-27 18:49:46 +0530

    update webcam and bump version number :rocket:
    Signed-off-by: Sambhav Jain <sambhavjain2612@gmail.com>

---
+++ index.js
@@ -0,0 +1,9 @@
+const Webcam = require('webcamjs');
+
+Webcam.attach('#video');
+
+function snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById('my_result').innerHTML = '<img src="' + data_uri + '"/>';
+    });
+}
--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-Webcam.attach('#video');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
 
-function snapshot() {
-    Webcam.snap(function (data_uri) {
-        document.getElementById('my_result').innerHTML = '<img src="' + data_uri + '"/>';
-    });
-}
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.set('port', PORT);
+
+const server = http.createServer(app);
+server.listen(PORT, () => console.log(`App running on localhost:${PORT}`));
